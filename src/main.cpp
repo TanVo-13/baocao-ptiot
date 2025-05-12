ea6b6b6ee9e4b6be77e6143821914d127e8b9969
phohoccode <qviet092@gmail.com>
2025-05-12 21:14:31 +0700

    xây dựng chức năng tự động bật tắt điều hòa

--- src/main.cpp
+++ src/main.cpp
@@ -5,54 +5,58 @@
 #include "DHT.h"
 #include <ArduinoJson.h>
 
-const char * MQTTServer = "broker.emqx.io";
-const char * MQTT_Topic = "HeThongNhaThongMinh";
+const char *MQTTServer = "broker.emqx.io";
+const char *MQTT_Topic = "HeThongNhaThongMinh";
 
 // Tạo ID ngẫu nhiên tại: https://www.guidgen.com/
-const char * MQTT_ID = "c868b704-fd6b-44b0-8e69-b6da2c63496b";
+const char *MQTT_ID = "c868b704-fd6b-44b0-8e69-b6da2c63496b";
 int Port = 1883;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-//Khai báo lcd
-LiquidCrystal_I2C lcd(0x27, 20, 4); //Địa chỉ I2C phổ biến là 0x27 hoặc 0x3F
+// Khai báo lcd
+LiquidCrystal_I2C lcd(0x27, 20, 4); // Địa chỉ I2C phổ biến là 0x27 hoặc 0x3F
 
-//Khai báo chân
-//Trang
-#define DHTPIN 23//Cảm biến dht22
+// Khai báo chân
+// Trang
+#define DHTPIN 23     // Cảm biến dht22
 #define DHTTYPE DHT22 // Loại cảm biến
 DHT dht(DHTPIN, DHTTYPE);
-#define khigas 35 //Cảm biến mq-2
-//Tân
-#define echoPin 25   //Cảm biến hcsr-04
+#define khigas 35 // Cảm biến mq-2
+// Tân
+#define echoPin 25 // Cảm biến hcsr-04
 #define trigPin 26
-#define anhsang 32 //Cảm biến ldr
-//Việt
-#define doamdat 34 //Cảm biến độ ẩm đất (potentiometer)
-#define mua 17 //Cảm biến mưa (button)
+#define anhsang 32 // Cảm biến ldr
+// Việt
+#define doamdat 34 // Cảm biến độ ẩm đất (potentiometer)
+#define mua 17     // Cảm biến mưa (button)
 
-//Khai bán chân thiết bị
-//Trang
-#define dieuhoa 18 //Máy điều hòa (relay module)
-#define coi 19 // Buzzer
-//Việt
-#define dirPin 2  // Motor tưới nước(Stepper)
+// Khai bán chân thiết bị
+// Trang
+#define dieuhoa 18 // Máy điều hòa (relay module)
+#define coi 19     // Buzzer
+// Việt
+#define dirPin 2 // Motor tưới nước(Stepper)
 #define stepPin 0
 #define sleepPin 4
 #define resetPin 16
 #define maiche 15 // Servo
-//Tân
+// Tân
 #define ledPin 33 // Đèn
 #define dirPin 27 // Motor bơm nước hồ bơi(Stepper)
 #define stepPin 14
 #define sleepPin 12
 #define resetPin 13
 
-void WIFIConnect() {
+bool automodeDht = true;
+
+void WIFIConnect()
+{
   Serial.println("Connecting to SSID: Wokwi-GUEST");
   WiFi.begin("Wokwi-GUEST", "");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -62,15 +66,23 @@ void WIFIConnect() {
   Serial.println(WiFi.localIP());
 }
 
-void MQTT_Reconnect() {
-  while (!client.connected()) {
-    if (client.connect(MQTT_ID)) {
+void MQTT_Reconnect()
+{
+  while (!client.connected())
+  {
+    if (client.connect(MQTT_ID))
+    {
       Serial.print("MQTT Topic: ");
       Serial.print(MQTT_Topic);
       Serial.print(" connected");
       client.subscribe(MQTT_Topic);
+      client.subscribe("HeThongNhaThongMinh/DHT22/Control/AC");
+      client.subscribe("HeThongNhaThongMinh/DHT22/Control/AC");
+      client.subscribe("HeThongNhaThongMinh/DHT22/Control/automodeDht");
       Serial.println("");
-    } else {
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -79,31 +91,56 @@ void MQTT_Reconnect() {
   }
 }
 
-void callback(char* topic, byte* message, unsigned int length) {
+void callback(char *topic, byte *message, unsigned int length)
+{
   Serial.print("Message arrived on topic: ");
   Serial.println(topic);
   Serial.print("Message: ");
   String stMessage;
 
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)message[i]);
     stMessage += (char)message[i];
   }
   Serial.println();
-  if (stMessage == "on") {
-    digitalWrite(ledPin, HIGH);
+
+  if (String(topic) == "HeThongNhaThongMinh/DHT22/Control/AC")
+  {
+    if (stMessage == "ON")
+    {
+      digitalWrite(dieuhoa, HIGH);
+      digitalWrite(ledPin, HIGH);
+    }
+    else if (stMessage == "OFF")
+    {
+      digitalWrite(dieuhoa, LOW);
+      digitalWrite(ledPin, LOW);
+    }
   }
-  else if (stMessage == "off") {
-    digitalWrite(ledPin, LOW);
+
+  if (String(topic) == "HeThongNhaThongMinh/DHT22/Control/automodeDht")
+  {
+    if (stMessage == "ON")
+    {
+      automodeDht = true;
+      Serial.println("Chuc nang tu dong da duoc bat");
+    }
+    else if (stMessage == "OFF")
+    {
+      automodeDht = false;
+      Serial.println("Chuc nang tu dong da duoc tat");
+    }
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   // Khởi tạo LCD
-  lcd.init(); 
-  lcd.backlight(); 
+  lcd.init();
+  lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("Dang ket noi WiFi");
 
@@ -125,15 +162,15 @@ void setup() {
   pinMode(anhsang, INPUT);
   pinMode(ledPin, OUTPUT);
 
-  pinMode(27, OUTPUT);  // dirPin - motor hồ bơi
-  pinMode(14, OUTPUT);  // stepPin
-  pinMode(12, OUTPUT);  // sleepPin
-  pinMode(13, OUTPUT);  // resetPin
+  pinMode(27, OUTPUT); // dirPin - motor hồ bơi
+  pinMode(14, OUTPUT); // stepPin
+  pinMode(12, OUTPUT); // sleepPin
+  pinMode(13, OUTPUT); // resetPin
 
   // Việt
   pinMode(doamdat, INPUT);
   pinMode(mua, INPUT);
-  pinMode(dirPin, OUTPUT);    // motor tưới cây
+  pinMode(dirPin, OUTPUT); // motor tưới cây
   pinMode(stepPin, OUTPUT);
   pinMode(sleepPin, OUTPUT);
   pinMode(resetPin, OUTPUT);
@@ -147,15 +184,18 @@ void setup() {
   lcd.print(WiFi.localIP());
 }
 
-void loop() {
+void loop()
+{
   delay(10);
-  if (!client.connected()) {
+  if (!client.connected())
+  {
     MQTT_Reconnect();
   }
   client.loop();
 
   static unsigned long lastSend = 0;
-  if (millis() - lastSend > 1000) {
+  if (millis() - lastSend > 2000)
+  {
     lastSend = millis();
 
     float nhietdo = dht.readTemperature();
@@ -174,14 +214,43 @@ void loop() {
     long duration = pulseIn(echoPin, HIGH);
     float khoangcach = duration * 0.034 / 2;
 
-    if (!isnan(nhietdo) && !isnan(doam)) {
-      Serial.print("Nhiet do: "); Serial.print(nhietdo); Serial.print(" *C, ");
-      Serial.print("Do am: "); Serial.print(doam); Serial.print(" %, ");
-      Serial.print("Khi gas: "); Serial.print(mq2_value); Serial.print(", ");
-      Serial.print("Cuong do anh sang: "); Serial.print(ldr_value); Serial.print(", ");
-      Serial.print("Do am dat: "); Serial.print(doamdat_value); Serial.print(", ");
-      Serial.print("Khoang cach: "); Serial.print(khoangcach); Serial.print(" cm, ");
-      Serial.print("Mua: "); Serial.println(trangthai_mua);
+    // Kiểm tra nhiệt độ và điều khiển điều hòa
+    if (!isnan(nhietdo) && automodeDht)
+    {
+      if (nhietdo > 30)
+      {
+        digitalWrite(dieuhoa, HIGH);
+        Serial.println("Dieu hoa dang bat");
+      }
+      else
+      {
+        digitalWrite(dieuhoa, LOW);
+        Serial.println("Dieu hoa dang tat");
+      }
+    }
+
+    if (!isnan(nhietdo) && !isnan(doam))
+    {
+      Serial.print("Nhiet do: ");
+      Serial.print(nhietdo);
+      Serial.print(" *C, ");
+      Serial.print("Do am: ");
+      Serial.print(doam);
+      Serial.print(" %, ");
+      Serial.print("Khi gas: ");
+      Serial.print(mq2_value);
+      Serial.print(", ");
+      Serial.print("Cuong do anh sang: ");
+      Serial.print(ldr_value);
+      Serial.print(", ");
+      Serial.print("Do am dat: ");
+      Serial.print(doamdat_value);
+      Serial.print(", ");
+      Serial.print("Khoang cach: ");
+      Serial.print(khoangcach);
+      Serial.print(" cm, ");
+      Serial.print("Mua: ");
+      Serial.println(trangthai_mua);
 
       // Tạo JSON để gửi đi
       StaticJsonDocument<512> doc;
@@ -192,11 +261,14 @@ void loop() {
       doc["doamdat"] = doamdat_value;
       doc["khoangcach"] = khoangcach;
       doc["mua"] = trangthai_mua;
+      doc["automodeDht"] = automodeDht;
 
       char buffer[256];
       serializeJson(doc, buffer);
       client.publish(MQTT_Topic, buffer);
-    } else {
+    }
+    else
+    {
       Serial.println("Doc du lieu that bai!");
     }
   }
